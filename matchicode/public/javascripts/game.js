b27d56f09189c8559d8c54e580b903ace3a6d65e
Jesse Lifshitz <jesse.lifshitz@gmail.com>
2015-11-15 00:48:03 -0500

    letters centered

--- matchicode/public/javascripts/game.js
+++ matchicode/public/javascripts/game.js
@@ -9,21 +9,50 @@
     document.getElementById('startForm').classList.toggle('hidden');
     //create game board
     createGameBoard(num);
-    console.log(num);
   }
   function createGameBoard(num){
     var symbols = generateUnicode(num);
-
     function generateUnicode(num){
-      var symbols = [];
+      var symbols = ['☀' , '☁' , '☂' , '☃', '★', '☎', '☕', '☘'],
+          sym = [];
       while (num > 0){
-        var sym = Math.floor(Math.random() * (9884 - 9728)) + 9728;
-        if (symbols.indexOf(sym) <= 0){
-          symbols.push('&#' + sym);
-          num--;
-        }
+        var random = Math.floor(Math.random() * (symbols.length - 0)) + 0;
+        sym.push(symbols[random]);
+        symbols.splice(random, 1);
+        num--;
       }
-      return symbols;
+      //double the symbols
+      return sym.concat(sym);
+    }
+    function createCards(sym){
+      for (var i = 0; i < sym.length; i++){
+        var card = createElement('div', 'card', null ,[createElement('p', 'symbol' ,sym[i])]);
+        card.addEventListener('click', cardClick);
+        document.getElementById('game').appendChild(card);
+      }
+
+
+
+      function createElement(type, classes, text, children){
+        var card = document.createElement(type);
+        if (classes){
+         card.classList.toggle(classes);
+        }
+        if (text){
+          card.appendChild(document.createTextNode(text));
+        }
+        if (children){
+           for (var i = 0; i < children.length; i++){
+            card.appendChild(children[i]);
+          }
+        }
+        return card;
+      }
+    }
+    var cards = createCards(symbols);
+
+    function cardClick(evt){
+      console.log('card clicked');
     }
   }
 })();
