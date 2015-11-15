90b1fa0b8fa500b94dc5cf8acf2123f23fc21164
Jesse Lifshitz <jesse.lifshitz@gmail.com>
2015-11-15 12:42:31 -0500

    adding board

--- matchicode/public/javascripts/game.js
+++ matchicode/public/javascripts/game.js
@@ -22,27 +22,52 @@
         num--;
       }
       //double the symbols
-      return sym.concat(sym);
+      sym = sym.concat(sym);
+      //shuffle
+      finalSym = [];
+      while (sym.length > 0){
+        var n = Math.floor(Math.random() * (sym.length - 0)) + 0;
+        finalSym.push(sym.splice(n, 1));
+      }
+      return finalSym;
     }
     function createCards(sym){
       for (var i = 0; i < sym.length; i++){
-        var card = createElement('div', 'card', null ,[createElement('p', 'symbol' ,sym[i])]);
+        var card = createElement('div', ['card'], null ,[createElement('p', ['symbol','hidden'] ,sym[i])]);
         card.addEventListener('click', cardClick);
+        if (checkGrid(sym, i)){
+          document.getElementById('game').appendChild(createElement('br'));
+        }
         document.getElementById('game').appendChild(card);
       }
 
-
+      function checkGrid(sym, i){
+        if (sym.length > 9 && ((i === 4)||(i === 8)||(i === 12))){
+          console.log(sym.length, i);
+          return true;
+        }
+        else if ((sym.length <= 9) && (sym.length > 4) && ((i === 3)||(i === 6)||(i === 9))){
+          console.log(sym.length, i);
+          return true;
+        }
+        else if (sym.length <= 4 && (i === 2)){
+          return true;
+        }
+        return false;
+      }
 
       function createElement(type, classes, text, children){
         var card = document.createElement(type);
         if (classes){
-         card.classList.toggle(classes);
+          for (var i = 0; i < classes.length; i++){
+            card.classList.toggle(classes[i]);
+          }
         }
         if (text){
           card.appendChild(document.createTextNode(text));
         }
         if (children){
-           for (var i = 0; i < children.length; i++){
+          for (var i = 0; i < children.length; i++){
             card.appendChild(children[i]);
           }
         }
@@ -52,7 +77,8 @@
     var cards = createCards(symbols);
 
     function cardClick(evt){
-      console.log('card clicked');
+      console.log('card clicked', evt);
+      console.log(evt.target.children[0].childNodes[0].nodeValue);
     }
   }
 })();
